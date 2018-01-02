81c6eed24928224eea86e6b7dd7e7773d62c7cfd
Modernnecro <rvemaker@hotmail.com>
2018-01-02 15:08:13 +0000

    Hello message to existing members added

--- applebot.py
+++ applebot.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3.6
 import logging
+import random
+import asyncio
+import requests
 
 import discord
 from discord.ext import commands
@@ -9,6 +12,27 @@ import discord.utils as utils
 logging.basicConfig(level='INFO')
 
 bot = commands.Bot(command_prefix='-')
+
+response = requests.get('https://status.discordapp.com/api/v2/summary.json')
+data = response.json()
+
+timeout_buckets = 0
+def add_bucket():
+    # Have to do this to be allowed to mutate it
+    global timeout_buckets
+    timeout_buckets += 1
+
+    # Async callback to perform in the background
+    async def decrement():
+        # Sleep 10 minutes
+        await asyncio.sleep(10 * 60)
+        # Remove the bucket
+        global timeout_buckets
+        timeout_buckets -= 1
+
+    # Runs the coroutine in the background without
+    # waiting for it to finish
+    asyncio.ensure_future(decrement())
 
 def get_general(guild):
     chan = utils.find(lambda c: c.name =='general', guild.channels)
@@ -25,6 +49,19 @@ async def talk(ctx, *, msg):
     """
     await ctx.message.delete()
     await ctx.send(msg)
+
+@bot.listen()
+async def on_message(message):
+    # random.random() returns a number in the range [0, 1].
+    is_hello = message.content.lower().startswith('hello')
+    # To access global scope, we don't have to add the global line.
+    # Yeah... still haven't quite sussed the point of that one xD
+    is_on_timeout = timeout_buckets > 0
+    is_permitted = random.random() < 0.25
+
+    if is_hello and is_permitted and not is_on_timeout:    
+        await message.channel.send('Hiya!')
+        add_bucket()
 
 @bot.listen()
 async def on_member_join(member):
